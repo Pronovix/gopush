629d690eabbe7f3bd25cc27d2a87c7fce5cfbe75
Tamás Demeter-Haludka <yorirou@gmail.com>
2012-10-22 22:35:27 +0200

    Adds jsonp support for the ping fallback.

--- src/gopush/ping.go
+++ src/gopush/ping.go
@@ -1,6 +1,7 @@
 package gopush
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/url"
@@ -14,13 +15,22 @@ func (svc *GoPushService) handlePing(w http.ResponseWriter, r *http.Request) {
 
 	v, _ := url.ParseQuery(r.URL.RawQuery)
 	center := v.Get("center")
+	callback := v.Get("callback") // For JSONP
+
 
 	if _, ok := svc.lastState[center]; center == "" || !ok {
 		serve404(w)
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, svc.lastState[center])
+	if callback == "" { // Normal response
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, svc.lastState[center])
+	} else { // JSONP response
+		w.Header().Set("Content-Type", "application/javascript; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		marshaled, _ := json.Marshal(svc.lastState[center])
+		io.WriteString(w, callback + "(" + string(marshaled) + ");")
+	}
 }
