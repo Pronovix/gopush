3c9861a191647e97fc2bb376878d899cf7827930
Tamás Demeter-Haludka <yorirou@gmail.com>
2012-10-22 22:10:28 +0200

    Improves CPU handling.

--- src/server/server.go
+++ src/server/server.go
@@ -1,7 +1,22 @@
 package main
 
-import "gopush"
+import (
+	"flag"
+	"runtime"
+
+	"gopush"
+)
+
+var procs = flag.Int("procs", 0, "Number of logical processors to utilize")
 
 func main() {
+	flag.Parse()
+
+	if *procs == 0 {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	} else {
+		runtime.GOMAXPROCS(*procs)
+	}
+
 	gopush.NewService("config.json", false).Start(":8080")
 }
