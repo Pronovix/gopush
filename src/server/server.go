5af6735137ee4ef4a68cb749a34aff481496128d
Tamás Demeter-Haludka <yorirou@gmail.com>
2012-11-05 15:13:43 +0100

    Adds a nicer way to shut down the server and profiling.

--- src/server/server.go
+++ src/server/server.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"flag"
+	"os"
+	"os/signal"
 	"runtime"
+	"runtime/pprof"
+
+	"log"
 
 	"gopush"
 )
@@ -12,6 +17,7 @@ var certFile = flag.String("certfile", "", "Certificate file (HTTPS, WSS)")
 var keyFile = flag.String("keyfile", "", "Private key (HTTPS, WSS)")
 var addr = flag.String("addr", "", "Network address of the server")
 var configName = flag.String("config", "config.json", "Path to the server config file.")
+var cpuProfile = flag.String("cpuprofile", "", "Enable CPU Profiling and write the results to a file.")
 
 func main() {
 	flag.Parse()
@@ -21,6 +27,27 @@ func main() {
 	} else {
 		runtime.GOMAXPROCS(*procs)
 	}
+
+	if *cpuProfile != "" {
+		f, err := os.Create(*cpuProfile)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func () {
+		for sig := range c {
+			log.Printf("Received signal %d, shutting down\n", sig)
+			if *cpuProfile != "" { // Stop profiling
+				pprof.StopCPUProfile()
+			}
+			os.Exit(1)
+		}
+	}()
 
 	var defaultAddr string
 
