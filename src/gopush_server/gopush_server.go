8d30633cf2488e3e215851a10492d9096dc814db
Tamás Demeter-Haludka <yorirou@gmail.com>
2012-11-07 16:24:29 +0100

    Adds ability to point the logger to a file.

--- src/gopush_server/gopush_server.go
+++ src/gopush_server/gopush_server.go
@@ -15,6 +15,7 @@ import (
 var procs = flag.Int("procs", 0, "Number of logical processors to utilize")
 var configName = flag.String("config", "config.json", "Path to the server config file.")
 var cpuProfile = flag.String("cpuprofile", "", "Enable CPU Profiling and write the results to a file.")
+var logFile = flag.String("log", "", "Logfile location. Empty string means stdout.")
 
 func main() {
 	flag.Parse()
@@ -23,6 +24,14 @@ func main() {
 		runtime.GOMAXPROCS(runtime.NumCPU())
 	} else {
 		runtime.GOMAXPROCS(*procs)
+	}
+
+	if *logFile != "" {
+		f, err := os.OpenFile(*logFile, os.O_CREATE | os.O_APPEND | os.O_RDWR, 0666)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		log.SetOutput(f)
 	}
 
 	if *cpuProfile != "" {
